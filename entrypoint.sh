e279af9a2c4f9e1cff9bb76344e9a971fef6c7ee
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2019-07-23 11:16:49 +0200

    feat: use non root user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 exe_winamax="/root/.wine/drive_c/users/root/Winamax/Winamax Poker/Winamax Poker.exe"
-exe_install="/WinamaxInstall.exe"
+exe_install="/wamInstall.exe"
 if [ ! -f "$exe_winamax" ]; then
-  wine32 "$exe_install"
-  wine "$exe_install"
+  wine32 "$exe_install" || wine "$exe_install"
 fi
 
 wine "$exe_winamax"
